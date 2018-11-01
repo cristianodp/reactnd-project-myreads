c83185f3999816c9a2825654023fa0401418aa08
Cristiano Diniz Pinto <cristianodp@gmail.com>
2018-11-01 03:14:06 +0100

    Commented activite indicator

--- src/views/ListBooks.js
+++ src/views/ListBooks.js
@@ -1,30 +1,33 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 import ListBooksContent from "./ListBooksContent";
 import FloatButtom from "./FloatButtom";
 
 class ListBooks extends React.Component {
-
-  showSearchPage = ()=>{
+  showSearchPage = () => {
     this.props.handlerShowSearchPage(true);
-  }
+  };
 
   render() {
-  
     return (
       <div className="list-books">
         <div className="list-books-title">
           <h1>MyReads</h1>
         </div>
-        <ListBooksContent books={this.props.books}
-          handlerChangeShelf={this.props.handlerChangeShelf}/>
+        <ListBooksContent
+          books={this.props.books}
+          handlerChangeShelf={this.props.handlerChangeShelf}
+        />
         <FloatButtom>
           <Link to="/search">Add a book</Link>
         </FloatButtom>
-        <div className="loader-container">
-        <div className="loader"></div>
-        </div>
+        {/* <div className="loader-container">
+          <div className="loader-backgroud">
+            backgroud
+          </div>
+          <div className="loader" /> 
+        </div>*/}
       </div>
     );
   }
